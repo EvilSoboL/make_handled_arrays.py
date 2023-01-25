2fe2906dc0913951bf2329bcaeb80c3549048822
Mark_Tarulin <evilsobol01@gmail.com>
2023-01-25 17:58:22 +0700

    Реализован запуск gnuplot через Python

--- plot_gnuplot.py
+++ plot_gnuplot.py
@@ -3,6 +3,8 @@ import os
 import subprocess
 
 script_file = os.listdir(Path("gnuplot_script"))
+print(script_file)
 full_path_to_script = Path("gnuplot_script", "{}".format(script_file[0])).absolute()
-result = subprocess.run(["gnuplot", "-c", "{}".format(full_path_to_script), "-platform wayland"])
-
+print(full_path_to_script)
+#result = subprocess.run(["gnuplot", "-c", "{}".format(full_path_to_script), "-platform wayland"])
+result = subprocess.run(["gnuplot", "-c", "{}".format(full_path_to_script), "-platform wayland"], shell=True)
