80c8e899b9c7800a3f19bf9cb247d07ab6b35e83
mark <evilsobol01@gmail.com>
2023-01-26 11:44:07 +0700

    Реализовано определение системы и корректный запуск скрипта

--- plot_gnuplot.py
+++ plot_gnuplot.py
@@ -3,8 +3,9 @@ import os
 import subprocess
 
 script_file = os.listdir(Path("gnuplot_script"))
-print(script_file)
 full_path_to_script = Path("gnuplot_script", "{}".format(script_file[0])).absolute()
-print(full_path_to_script)
-#result = subprocess.run(["gnuplot", "-c", "{}".format(full_path_to_script), "-platform wayland"])
-result = subprocess.run(["gnuplot", "-c", "{}".format(full_path_to_script), "-platform wayland"], shell=True)
+
+if os.name == "posix":
+    result = subprocess.run(["gnuplot", "-c", "{}".format(full_path_to_script), "-platform wayland"])
+elif os.name == "nt":
+    result = subprocess.run(["gnuplot", "-c", "{}".format(full_path_to_script), "-platform wayland"], shell=True)
